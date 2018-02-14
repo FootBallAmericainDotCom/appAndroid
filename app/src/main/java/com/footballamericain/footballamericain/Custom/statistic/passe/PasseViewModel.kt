1849042a424faeed4a378c30828abcecb0204776
Jean-Christian Mbappe Besseme <jc.mbappe@gmail.com>
2018-02-14 22:25:32 +0100

    [MASTER] Passe and Run ViewModel

---
+++ app/src/main/java/com/footballamericain/footballamericain/Custom/statistic/passe/PasseViewModel.kt
@@ -0,0 +1,20 @@
+package com.footballamericain.footballamericain.Custom.statistic.passe
+
+import android.databinding.ObservableField
+
+/**
+ * Created by Jc on 14/02/2018.
+ */
+class PasseViewModel {
+    val completedPasse = ObservableField<String>()
+    val triedPasse = ObservableField<String>()
+    val yardGain = ObservableField<String>()
+
+    val percentageCompleted = ObservableField<String>()
+    val averageLenth = ObservableField<String>()
+    val longestPasse = ObservableField<String>()
+
+    val touchDown = ObservableField<String>()
+    val interception = ObservableField<String>()
+    val qbRating = ObservableField<String>()
+}
---
+++ app/src/main/java/com/footballamericain/footballamericain/Custom/statistic/run/RunViewModel.kt
@@ -0,0 +1,15 @@
+package com.footballamericain.footballamericain.Custom.statistic.run
+
+import android.databinding.ObservableField
+
+/**
+ * Created by Jc on 14/02/2018.
+ */
+class RunViewModel {
+    val runNumber = ObservableField<String>()
+    val yardGain = ObservableField<String>()
+    val averageLenth = ObservableField<String>()
+
+    val longestRun = ObservableField<String>()
+    val touchDown = ObservableField<String>()
+}
