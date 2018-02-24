25347ba6e4cf7682d9a7ec013f3cef5f0eb16df7
Jean-Christian Mbappe Besseme <jc.mbappe@gmail.com>
2018-02-24 14:47:58 +0100

    [MASTER] DepthChart View Holder

--- app/src/main/java/com/footballamericain/footballamericain/ViewHolder/depthChart/DepthChartViewHolder.kt
+++ app/src/main/java/com/footballamericain/footballamericain/ViewHolder/depthChart/DepthChartViewHolder.kt
@@ -1,7 +1,27 @@
 package com.footballamericain.footballamericain.ViewHolder.depthChart
 
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import com.footballamericain.footballamericain.databinding.ViewHolderDepthChartBinding
+import com.footballamericain.footballamericain.dummy.PlayerDummyContent
+
 /**
  * Created by Jc on 24/02/2018.
  */
-class DepthChartViewHolder {
+class DepthChartViewHolder(private val binding: ViewHolderDepthChartBinding)
+    : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(title: String, players: ArrayList<PlayerDummyContent.Player>, inflater: LayoutInflater?) {
+        val viewModel = DepthChartViewModel(title)
+
+        binding.viewModel = viewModel
+        binding.container.removeAllViews()
+
+        players.forEachIndexed { index, player ->
+            binding.container.addView(viewModel.createPlayerView(
+                    inflater, binding.container,
+                    player, index
+            ))
+        }
+    }
 }
--- app/src/main/java/com/footballamericain/footballamericain/ViewHolder/depthChart/DepthChartViewModel.kt
+++ app/src/main/java/com/footballamericain/footballamericain/ViewHolder/depthChart/DepthChartViewModel.kt
@@ -1,7 +1,25 @@
 package com.footballamericain.footballamericain.ViewHolder.depthChart
 
+import android.databinding.ObservableField
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.footballamericain.footballamericain.ViewHolder.Player.PlayerViewModel
+import com.footballamericain.footballamericain.databinding.ViewHolderPlayerBinding
+import com.footballamericain.footballamericain.dummy.PlayerDummyContent
+
 /**
  * Created by Jc on 24/02/2018.
  */
-class DepthChartViewModel {
+class DepthChartViewModel(title: String) {
+    val title = ObservableField<String>(title)
+
+    fun createPlayerView(inflater: LayoutInflater?, parent: ViewGroup,
+                      player: PlayerDummyContent.Player, viewPosition: Int): View {
+        val binding = ViewHolderPlayerBinding.inflate(inflater, parent, false)
+        binding.model = PlayerViewModel(player.number, player.name,
+                player.position, viewPosition)
+
+        return binding.root
+    }
 }
